a4f540ff68169ec9490479e8e31ca65e84bc52df
Saikiran Rudra <saikiranrudra2@gmail.com>
2021-07-18 17:05:58 +0530

    cards for dashboard done

--- pages/user/index.js
+++ pages/user/index.js
@@ -2,22 +2,42 @@ import { useState } from "react";
 import { Typography, Button } from "@material-ui/core";
 import UserLayout from "../../src/components/layout/UserLayout";
 import CreateDelivery from "../../src/components/delivery/CreateDelivery";
+import { userAuthCheck } from "./../../src/utils/userAuthCheck";
+import Card from "../../src/components/utils/Card";
 
-const Home = () => {
+const Home = ({ user }) => {
   const [open, setOpen] = useState(false);
   return (
     <>
       <UserLayout route={"/user/"}>
         <div style={{ textAlign: "right", margin: ".8rem 0" }}>
-          <Button variant="contained" color="primary" onClick={() => { setOpen(true) }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
             Create Delivery
           </Button>
         </div>
-        <Typography variant="h1">Home Page</Typography>
+        <div>
+          <Card title="Created Deliveries">
+            <Typography variant="body">
+              this is the content of the card which is written here, and now i
+              am just typing some random text which dosnt make any sensen just
+              wrote to fill the card with some text
+            </Typography>
+          </Card>
+        </div>
       </UserLayout>
-      <CreateDelivery open={open} setOpen={setOpen} />
+      <CreateDelivery open={open} setOpen={setOpen} user={user} />
     </>
   );
+};
+
+export const getServerSideProps = async (ctx) => {
+  return await userAuthCheck(ctx);
 };
 
 export default Home;
---
+++ src/components/utils/Card.js
@@ -0,0 +1,29 @@
+import { Paper, Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  title: {
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+  container: {
+    padding: ".8rem 1rem",
+  },
+});
+
+const Card = ({ title, children }) => {
+  const classes = useStyles();
+  return (
+    <Paper className={classes.container}>
+      <Typography variant="h6" component="h2" className={classes.title}>
+        {title}
+      </Typography>
+      {children}
+    </Paper>
+  );
+};
+
+Card.defaultProps = {
+  title: "No Title",
+};
+
+export default Card;
